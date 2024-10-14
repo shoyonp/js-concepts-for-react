a08f52322fbdc8bc880297dabb2c716656d6440e
shoyonp <shoyonkumar143@gmail.com>
2024-10-15 02:03:58 +0600

    done

---
+++ notation.js
@@ -0,0 +1,23 @@
+const person = {
+    name: 'purna',
+    profession:'student',
+    age:'19',
+    16:'summer',
+    'husband-name': 'shoyon',
+    address:'amar kache'
+}
+
+// dot natation
+const prof1 = person.profession;
+
+// bracket notation
+const prof2 = person['profession'];
+const pName = 'profession'
+const prof3 = person[pName];
+// console.log(prof3);
+
+const season = person[16];
+// console.log(season);
+
+const husband = person['husband-name'];
+console.log(husband);
